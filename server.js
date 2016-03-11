9f8b933bc11f33c124878bfcec078349bb79e7ed
Yahel Nachum <ynachum@wpi.edu>
2016-03-10 19:16:42 -0500

    navigation bar seperated into individual html file

--- server.js
+++ server.js
@@ -12,7 +12,19 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  var navBarHTML = fs.readFileSync(path.join(__dirname, '/public/nav_bar.html'));
+  var htmlPage = fs.readFileSync(path.join(__dirname, '/public/home.html')).toString();
+  htmlPage = htmlPage.replace('INSERT_NAV_BAR_HERE', navBarHTML);
+  res.write(htmlPage);
+  res.end();
+});
+
+app.get('/recipes', function(req, res) {
+  var navBarHTML = fs.readFileSync(path.join(__dirname, '/public/nav_bar.html'));
+  var htmlPage = fs.readFileSync(path.join(__dirname, '/public/recipes.html')).toString();
+  htmlPage = htmlPage.replace('INSERT_NAV_BAR_HERE', navBarHTML);
+  res.write(htmlPage);
+  res.end();
 });
 
 app.listen(port, function() {
