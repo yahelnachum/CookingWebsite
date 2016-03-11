9ca220e9fa6a4d97ad699748425e17ebb221aba1
Yahel Nachum <ynachum@wpi.edu>
2016-03-10 19:33:31 -0500

    cleaned up code and pushed repetitive stuff into functions

--- server.js
+++ server.js
@@ -12,17 +12,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
-  var navBarHTML = fs.readFileSync(path.join(__dirname, '/public/nav_bar.html'));
   var htmlPage = fs.readFileSync(path.join(__dirname, '/public/home.html')).toString();
-  htmlPage = htmlPage.replace('INSERT_NAV_BAR_HERE', navBarHTML);
+  htmlPage = addCommonPageElements(htmlPage);
   res.write(htmlPage);
   res.end();
 });
 
-app.get('/recipes', function(req, res) {
+function addCommonPageElements(htmlPage){
   var navBarHTML = fs.readFileSync(path.join(__dirname, '/public/nav_bar.html'));
-  var htmlPage = fs.readFileSync(path.join(__dirname, '/public/recipes.html')).toString();
-  htmlPage = htmlPage.replace('INSERT_NAV_BAR_HERE', navBarHTML);
+  var commonHeadElements = fs.readFileSync(path.join(__dirname, '/public/common_head_elements.html'));
+  htmlPage = htmlPage.replace('INSERT_COMMON_HEAD_ELEMENTS_HERE', commonHeadElements);
+  return htmlPage.replace('INSERT_NAV_BAR_HERE', navBarHTML);
+}
+
+app.get('/food', function(req, res) {
+  var htmlPage = fs.readFileSync(path.join(__dirname, '/public/food.html')).toString();
+  htmlPage = addCommonPageElements(htmlPage);
   res.write(htmlPage);
   res.end();
 });
